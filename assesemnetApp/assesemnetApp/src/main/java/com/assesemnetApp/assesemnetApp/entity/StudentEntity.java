c6f2bf3c018c159b5cd41fc741fe21388b15411b
mrutyunjaykumar31 <mrityunjaykumar31@gmail.com>
2023-05-07 00:23:47 +0530

    massage

--- assesemnetApp/assesemnetApp/src/main/java/com/assesemnetApp/assesemnetApp/entity/StudentEntity.java
+++ assesemnetApp/assesemnetApp/src/main/java/com/assesemnetApp/assesemnetApp/entity/StudentEntity.java
@@ -15,8 +15,109 @@ public class StudentEntity {
 	private String studentFirstname;
 	private String studentLastname;
 	private String studentEnrollmentNo;
+	private Long studentMobileNumber;
+	private String studentFatherName;
+	private String studentaddress;
+	private String studentEmail;
+	private String studentBranch;
+	private String studentClientId;
+	private String studentSetAssign;
+	private String studentStatus;
+	private Long studentMarks;
+	private String studentGender;
+	private String studentState;
+	
 	
 
+	
+	
+	public StudentEntity(Long studentId, String studentFirstname, String studentLastname, String studentEnrollmentNo,
+			Long studentMobileNumber, String studentFatherName, String studentaddress, String studentEmail,
+			String studentBranch, String studentClientId, String studentSetAssign, String studentStatus,
+			Long studentMarks, String studentGender, String studentState) {
+		super();
+		this.studentId = studentId;
+		this.studentFirstname = studentFirstname;
+		this.studentLastname = studentLastname;
+		this.studentEnrollmentNo = studentEnrollmentNo;
+		this.studentMobileNumber = studentMobileNumber;
+		this.studentFatherName = studentFatherName;
+		this.studentaddress = studentaddress;
+		this.studentEmail = studentEmail;
+		this.studentBranch = studentBranch;
+		this.studentClientId = studentClientId;
+		this.studentSetAssign = studentSetAssign;
+		this.studentStatus = studentStatus;
+		this.studentMarks = studentMarks;
+		this.studentGender = studentGender;
+		this.studentState = studentState;
+	}
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+	public String getStudentBranch() {
+		return studentBranch;
+	}
+	public void setStudentBranch(String studentBranch) {
+		this.studentBranch = studentBranch;
+	}
+	public String getStudentClientId() {
+		return studentClientId;
+	}
+	public void setStudentClientId(String studentClientId) {
+		this.studentClientId = studentClientId;
+	}
+	public String getStudentSetAssign() {
+		return studentSetAssign;
+	}
+	public void setStudentSetAssign(String studentSetAssign) {
+		this.studentSetAssign = studentSetAssign;
+	}
+	public String getStudentStatus() {
+		return studentStatus;
+	}
+	public void setStudentStatus(String studentStatus) {
+		this.studentStatus = studentStatus;
+	}
+	public Long getStudentMarks() {
+		return studentMarks;
+	}
+	public void setStudentMarks(Long studentMarks) {
+		this.studentMarks = studentMarks;
+	}
+	public String getStudentGender() {
+		return studentGender;
+	}
+	public void setStudentGender(String studentGender) {
+		this.studentGender = studentGender;
+	}
+	public String getStudentState() {
+		return studentState;
+	}
+	public void setStudentState(String studentState) {
+		this.studentState = studentState;
+	}
+	public String getStudentaddress() {
+		return studentaddress;
+	}
+	public void setStudentaddress(String studentaddress) {
+		this.studentaddress = studentaddress;
+	}
+	public String getStudentFatherName() {
+		return studentFatherName;
+	}
+	public void setStudentFatherName(String studentFatherName) {
+		this.studentFatherName = studentFatherName;
+	}
+	public Long getStudentMobileNumber() {
+		return studentMobileNumber;
+	}
+	public void setStudentMobileNumber(Long studentMobileNumber) {
+		this.studentMobileNumber = studentMobileNumber;
+	}
 	public Long getStudentId() {
 		return studentId;
 	}
@@ -42,19 +143,17 @@ public class StudentEntity {
 		this.studentEnrollmentNo = studentEnrollmentNo;
 	}
 	
-	public StudentEntity(Long studentId, String studentFirstname, String studentLastname, String studentEnrollmentNo) {
-		super();
-		this.studentId = studentId;
-		this.studentFirstname = studentFirstname;
-		this.studentLastname = studentLastname;
-		this.studentEnrollmentNo = studentEnrollmentNo;
-	}
 	
 	public StudentEntity() {}
 	@Override
 	public String toString() {
 		return "StudentEntity [studentId=" + studentId + ", studentFirstname=" + studentFirstname + ", studentLastname="
-				+ studentLastname + ", studentEnrollmentNo=" + studentEnrollmentNo + "]";
+				+ studentLastname + ", studentEnrollmentNo=" + studentEnrollmentNo + ", studentMobileNumber="
+				+ studentMobileNumber + ", studentFatherName=" + studentFatherName + ", studentaddress="
+				+ studentaddress + ", studentEmail=" + studentEmail + ", studentBranch=" + studentBranch
+				+ ", studentClientId=" + studentClientId + ", studentSetAssign=" + studentSetAssign + ", studentStatus="
+				+ studentStatus + ", studentMarks=" + studentMarks + ", studentGender=" + studentGender
+				+ ", studentState=" + studentState + "]";
 	};
 	
 	
